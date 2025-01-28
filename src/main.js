2d5cd7f5e7cc9c0f61b9193df9e8ef25b15f3c9c
danielwidhiarto <danielwidhiarto@gmail.com>
2025-01-28 21:25:11 +0700

    setup rouiting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // Import the router
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router) // Tell Vue to use the router
+
+app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/Home.vue'
+
+// Define routes
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+]
+
+// Create the router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
