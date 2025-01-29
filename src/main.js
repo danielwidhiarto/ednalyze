d19827e261f9d0ce310f3f404f31d1fe9f268881
danielwidhiarto <danielwidhiarto@gmail.com>
2025-01-29 13:33:00 +0700

    change basic detail

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // Import the router
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router"; // Import the router
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router) // Tell Vue to use the router
+app.use(router); // Tell Vue to use the router
 
-app.mount('#app')
+app.mount("#app");
