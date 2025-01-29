7d141952b3daa7c2ae6ec09ceedc063e3f8457ed
danielwidhiarto <danielwidhiarto@gmail.com>
2025-01-29 14:34:02 +0700

    add navbar navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <div id="app">
-    <router-view class="form-container"></router-view>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="#">EDnalyze</a>
+        <div class="navbar-nav ms-auto">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/watchlist" class="nav-link">⭐ Watchlist</router-link>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-};
+  setup() {
+    return {}
+  },
+}
 </script>
+
+<style>
+/* Styling Navbar */
+.navbar-nav .nav-link {
+  font-weight: 500;
+  padding: 0.5rem 1rem;
+}
+
+.navbar-nav .nav-link:hover {
+  text-decoration: underline;
+}
+</style>
